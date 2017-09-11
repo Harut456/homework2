12b4d834406419a5e9efa57c962f71f807fe097d
Harutyun <you@example.com>
2017-09-11 23:08:36 +0400

    create assetts folder

---
+++ assets/homework2.js
@@ -0,0 +1,22 @@
+let num1 = 37;
+let num2 = 13;
+
+let result;
+let username = "Harut";
+
+let currentYear = 2017, myBirthDate = 1995, myAge;
+
+result = num1 + num2;
+console.log('result: num1 + num2 =', result);
+
+num2 = num1 + num2;
+console.log('new value num2: num1 + num2 =', num2);
+
+num1 = (num1 + num2)/2;
+console.log('new value num1: (num1 + num2)/2', num1);
+
+
+console.log('My name is ', username);
+
+myAge = currentYear - myBirthDate;
+console.log('My age is: currentYear - myBirthDate:', myAge);
